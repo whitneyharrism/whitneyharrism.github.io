0362155fe4f52c79fbccb84d14f9087938f1de91
whitneyharrism <whitneyharrism@gmail.com>
2019-04-12 15:29:45 -0600

    added temples

--- assignments/test/scripts/modalLogan.js
+++ assignments/test/scripts/modalLogan.js
@@ -1,17 +1,30 @@
-var trigger = document.getElementById('myImg2');
-var modal = document.querySelector(".modal2");
-var closeButton = document.querySelector(".close-button2");
+// var trigger = document.getElementById('myImg2');
+// var modal = document.querySelector(".modal2");
+// var closeButton = document.querySelector(".close-button2");
 
-function toggleModal() {
-    modal.classList.toggle("show-modal");
-}
+// function toggleModal() {
+//     modal.classList.toggle("show-modal");
+// }
 
-function windowOnClick(event) {
-    if (event.target === modal) {
-        toggleModal();
+// function windowOnClick(event) {
+//     if (event.target === modal) {
+//         toggleModal();
+//     }
+// }
+
+// trigger.addEventListener("click", toggleModal);
+// closeButton.addEventListener("click", toggleModal);
+// window.addEventListener("click", windowOnClick);
+
+newFunction();
+
+function newFunction() {
+    var btn = document.getElementsByClassName("click");
+    for (var i = 0; i < btn.length; i++) {
+        var thisBtn = btn[i];
+        thisBtn.addEventListener("click", function () {
+            var modal = document.getElementById(this.dataset.modal);
+            modal.style.display = "block";
+        }, false);
     }
 }
-
-trigger.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
